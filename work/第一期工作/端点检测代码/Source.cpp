6e3b830fa24415c48a8bd63e5ebd8109217f3abf
张立飞 <18852923073@163.com>
2017-01-03 20:48:21 +0800

    Add files via upload

---
+++ work/第一期工作/端点检测代码/Source.cpp
@@ -0,0 +1,14 @@
+#include "Wav_File_Handle.h"
+
+int main()
+{
+
+	FILE *fp;
+	if ((fp = fopen("Œ¢»Ìƒ¨»œ.wav", "rb")) == NULL) {
+		cout << "ERROR : File open failed !" << endl;
+		exit(-1);
+	}
+	Wav_File_Work temp(fp);
+	temp.ShowData();
+	return 0;
+}
